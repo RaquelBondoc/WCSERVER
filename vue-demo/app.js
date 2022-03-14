acfcfcfa56306d375d916afcc85547f266e72c62
Raquel Ann Bondoc <100080453+RaquelBondoc@users.noreply.github.com>
2022-03-14 22:27:06 +0800

    Add files via upload

---
+++ vue-demo/app.js
@@ -0,0 +1,6 @@
+const app = Vue.createApp({
+    // Create functions, data
+    template: "<h2> This is my website</h2>"
+});
+  
+app.mount('#app')
---
+++ vue-demo/app2.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    // Create functions, data
+    data(){
+        return{
+            title: 'The Ledger',
+            author: 'Raquel Ann Bondoc',
+            age: 20
+        }
+    }
+});
+
+app.mount('#app')
